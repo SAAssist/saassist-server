3d2ec5018a3cd07116ee4db2a5ddac7371f23db3
Kairo Araujo <kairoaraujo@gmail.com>
2017-01-04 14:39:40 +0100

    listing all current APARs

--- saassist-server.py
+++ saassist-server.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# 
-
+#
 
 import argparse
 from saassist.saaserver import SAAServer
+from server_config import saassist_home
+from os import listdir
 
+# version control
+version = 0.1
 
 # header
 def _print_header():
     print('\n')
     print('=' * 80)
-    print('SAAssist-server (Security APAR Assist Server) - Version 0.1-beta')
+    print('SAAssist-server (Security APAR Assist Server) - Version {0}'.format(
+        version
+    ))
     print('=' * 80)
 
 
@@ -20,16 +25,25 @@ _print_header()
 
 # construct the command lines for server
 parser = argparse.ArgumentParser()
+
+parser.add_argument('list', action='store_true',
+                    help='list all available CVE/IV on SAAssist')
+
 parser.add_argument('SecurityID', type=str,
                     help='is a CVE (Common Vulnerabilities and Exposures) or '
                          'IV (Interim Fix) existent on IBM FLRT (Fix Level '
-                         'Recommendation Tool)'
-                    )
+                         'Recommendation Tool)')
+
 parser.add_argument('-u', '--update', action='store_true',
-                    help='update a CVE already existent on SAAssistant')
+                    help='update a CVE/IV already existent on SAAssist')
+
 arguments = parser.parse_args()
 
 sec_id = arguments.SecurityID.upper()
+
+if arguments.list:
+    listdir('{0}/saassist/data/repos/'.format(saassit_home))
+    exit(0)
 
 # verify if the information looks correct
 if sec_id.startswith('CVE') and len(sec_id) == 13:
@@ -49,6 +63,7 @@ if sec_id_std is False:
     exit()
 
 # do the update action
+
 if arguments.update:
     saassist_run = SAAServer(sec_id)
     saassist_run.repo_creation(update=True)
